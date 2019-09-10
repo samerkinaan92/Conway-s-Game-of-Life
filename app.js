a1d689790437eb267d15234026f54b8da3dda2ea
samer kinaan <samerkinaan@gmail.com>
2019-09-10 12:35:56 +0300

    ready for deployment

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use("style", express.static(__dirname + '/style'));
+app.use("js", express.static(__dirname  + '/js'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + 'index.html'));
+})
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
